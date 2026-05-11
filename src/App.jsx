1aba625c199f1176978619a6217df27afed897e2
Siva <srk7710@gmail.com>
2026-05-10 23:05:17 -0400

    fix: add basename to React Router for gh-pages support

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import "./App.css";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <div className="app">
         <Navbar />
         <Routes>
